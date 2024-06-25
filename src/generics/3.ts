42fed406643108ca05c04386b7796e7732de4fb9
OlenaPanf <elena.el@ukr.net>
2024-06-26 02:05:41 +0300

    generics 4

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -4,7 +4,7 @@ function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
 
 export {};
 
-//========================================
+//============================================================================
 // function merge(objA, objB) {
 //   return Object.assign(objA, objB);
 // }
---
+++ src/generics/4.ts
@@ -0,0 +1,22 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+function createOrUpdateUser<T extends Partial<User>>(initialValues: T) {
+  // Оновлення користувача
+}
+
+createOrUpdateUser({
+  email: "user@mail.com",
+  password: "password123",
+});
+
+export {};
+
+//===========================================================
+// function createOrUpdateUser(initialValues: User) {
+//   // Оновлення користувача
+// }
