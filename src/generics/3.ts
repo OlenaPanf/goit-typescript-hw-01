bcf65d5bab4973350777e14e25c8f2d154287a4f
OlenaPanf <elena.el@ukr.net>
2024-06-26 01:57:54 +0300

    generics 1-3

---
+++ src/generics/3.ts
@@ -0,0 +1,10 @@
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return Object.assign(objA, objB);
+}
+
+export {};
+
+//========================================
+// function merge(objA, objB) {
+//   return Object.assign(objA, objB);
+// }
